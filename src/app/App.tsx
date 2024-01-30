1468ca4a8e19469accf42c79b88a3ad75636e020
Nikita <123288888+spyr0xz@users.noreply.github.com>
2024-01-30 17:51:32 +0300

    init commit

---
+++ src/app/App.tsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div>
+      There will be web-estate app
+    </div>
+  )
+}
+
+export default App
---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import "./app/styles/index.scss";
+import App from "./app/App";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
